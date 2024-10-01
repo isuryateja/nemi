de18dab369179ea4729b703ce8ee234ca39d080f
surya <surya.iriventi@gmail.com>
2024-10-01 08:14:48 +0530

    project init with taskEither

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,34 @@
 import express, { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
-import * as E from 'fp-ts/Either';
+import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
-import { Task } from 'fp-ts/Task';
 
 const prisma: PrismaClient = new PrismaClient();
 const app = express();
 app.use(express.json());
 
-const sendJson = (res: Response) => (data: object): Task<void> => () =>
-    new Promise<void>((resolve) => {
+const sendJson = (res: Response) => (data: object): TE.TaskEither<unknown, void> =>
+    TE.rightTask(() => new Promise<void>((resolve) => {
         res.json(data);
         resolve(); // Explicitly resolve void
-    });
+    }));
 
-const sendError = (res: Response) => (error: object): Task<void> => () =>
-    new Promise<void>((resolve) => {
+const sendError = (res: Response) => (error: object): TE.TaskEither<unknown, void> =>
+    TE.leftTask(() => new Promise<void>((resolve) => {
         res.status(500).json(error);
         resolve(); // Explicitly resolve void
-    });
+    }));
 
-app.get("/", (req: Request, res: Response): void => {
-    const responseTask: Task<void> = pipe(
-        E.right({ message: "Hello from Nemi!" }),
-        E.fold(
+app.get("/", async (req: Request, res: Response): Promise <void> => {
+    const responseTaskEither: TE.TaskEither<unknown, void> = pipe(
+        TE.right({ message: "Hello from Nemi!" }),
+        TE.fold(
             sendError(res),
             sendJson(res)
         )
     );
 
-    responseTask().catch(() =>
-        res.status(500).send({ body: "Something went wrong" })
-    );
+    await responseTaskEither();
 });
 
 const port: string | number = process.env.PORT || 3000;
